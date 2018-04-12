9ac63a327b28a58c0c788cfdb00508b5ac4372b2
Fabian Baena <fabbaena@gmail.com>
2018-04-12 22:19:03 +0000

    added CheckAgentCall

---
+++ CheckAgentCall/lambda_function.py
@@ -0,0 +1,24 @@
+import boto3
+import sys
+import os
+
+config_table = os.environ['config_table']
+db = boto3.resource('dynamodb', region_name='us-east-1')
+config = {}
+
+def handler(event, context):
+    global config
+    count = 0
+    try:
+        config = db.Table(config_table).get_item(Key={ 
+                "Key": "Config" 
+            })['Item']['Value']
+
+        db = boto3.resource('dynamodb', region_name='us-east-1')
+        out = db.Table(config['outboundQueueTable']).scan()
+        count = out['Count']
+    except Exception as e:
+        print "ERROR(%s:%d): %s %s" % (sys._getframe().f_code.co_name, 
+            sys._getframe().f_lineno, type(e).__name__, e.args[0])
+
+    return count
